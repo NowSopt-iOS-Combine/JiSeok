db7e1da8e29b57bff8545b3b337fb47876d9f07f
sozohoy <sozohoy@gmail.com>
2024-05-09 17:28:21 +0900

    feat: LoginViewModel

--- 34th-Sopt-Assignment/34th-Sopt-Assignment/ViewModel/LoginViewModel.swift
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/ViewModel/LoginViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Combine
+
+final class LoginViewModel {
+    @Published var idTextInput: String = ""
+    @Published var passwordTextInput: String = ""
+    lazy var isVaild: AnyPublisher<Bool, Never> = Publishers
+        .CombineLatest($idTextInput,
+                       $passwordTextInput)
+        .map {
+            if $0.isEmpty || $1.isEmpty {
+                return false
+            } else {
+                if $0.isIdVaild() && $1.isPasswordVaild() {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+}
